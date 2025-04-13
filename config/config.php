a03d0a52c6d175e0ef8c2ee8157dd01aa921b71c
bhaktofmahakal <moviesf14@gmail.com>
2025-04-13 12:34:31 +0530

    Files upload

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+    define("HOST", "localhost");
+    define("DBNAME", "restoran");
+    define("USER", "root");
+    define("PASS", "");
+    define("ADMINURL", "http://localhost/restoran/admin-panel");
